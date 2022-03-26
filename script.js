e9525060d0fd572fde2d6aadf62b47d8abba8de6
Evan Jones <evanjones125@gmail.com>
2022-03-26 15:21:24 -0600

    Create if / else chain

--- script.js
+++ script.js
@@ -1,28 +1,60 @@
 const computerPlay = () => {
     //randomly return either rock, paper or scissors
-    //generate number between 0 and 1
     //if num < 0.333, return rock, ...
     const ranNum = Math.random();
 
     if (ranNum <= 0.33333) {
-        return 1
+        return "rock";
     } else if (ranNum >= 0.33334 && ranNum <= 0.66666) {
-        return 2;
+        return "paper";
     } else if (ranNum >= 0.66667) {
-        return 3;
+        return "scissors";
     }
 }
 
 console.log(computerPlay());
 
 //get input from user; use it for playerSelection
-// let playerSelection = parseInt(prompt("enter your move: "))
+//create function that takes user input and converts it to num 1-3
+const userPlay = () => {
+    let playerInput = prompt("enter your move: ");
+    
+    if (playerInput == "rock") {
+        return "rock";
+    } else if (playerInput == "paper") {
+        return "paper";
+    } else if (playerInput == "scissors") {
+        return "scissors";
+    } else {
+        return null;
+    }
+}
+
+// console.log(userPlay());
+
+const computerSelection = computerPlay();
+const playerSelection = userPlay();
 
 const playRound = (playerSelection, computerSelection) => {
     //takes in player and computer selection
     //returns a string that declares a winner
 
     //if pS beats cS, return "You win!"
+    if (playerSelection === computerSelection) {
+        return `draw! you played ${playerSelection} and the computer played ${computerSelection}. enter another number`;
+    } else if (playerSelection === "rock" && computerSelection === "paper") {
+        return `computer wins this round. it played ${computerSelection}, which beats ${playerSelection}`;
+    } else if (playerSelection === "rock" && computerSelection === "scissors") {
+        return `you win this round. you played ${playerSelection}, which beats ${computerSelection}`;
+    } else if (playerSelection === "paper" && computerSelection === "rock") {
+        return `you win this round. you played ${playerSelection}, which beats ${computerSelection}`;
+    } else if (playerSelection === "paper" && computerSelection === "scissors") {
+        return `computer wins this round. it played ${computerSelection}, which beats ${playerSelection}`;
+    } else if (playerSelection === "scissors" && computerSelection === "paper") {
+        return `you win this round. you played ${playerSelection}, which beats ${computerSelection}`;
+    } else if (playerSelection === "scissors" && computerSelection === "rock") {
+        return `computer wins this round. it played ${computerSelection}, which beats ${playerSelection}`;
+    }
 }
 
 const game = (playRound) => {
@@ -32,6 +64,4 @@ const game = (playRound) => {
     //outputs a report on who won or lost
 }
 
-const playerSelection = "rock";
-const computerSelection = computerPlay();
 console.log(playRound(playerSelection, computerSelection));
