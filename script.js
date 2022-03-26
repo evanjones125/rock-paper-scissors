852c26983c78917ed9cbbcceff1120e0c330c2f5
Evan Jones <evanjones125@gmail.com>
2022-03-26 14:02:07 -0600

    Add functions and pseudocode

---
+++ script.js
@@ -0,0 +1,14 @@
+//rock paper scissors
+
+const computerPlay = () => {
+    //randomly return either rock, paper or scissors
+    //generate number between 1 and 3
+    //if 1, return rock, ...
+}
+
+const playRound = (playerSelection, computerSelection) => {
+    //takes in player and computer selection
+    //returns a string that declares a winner
+
+    //if pS beats cS, return "You win!"
+}
