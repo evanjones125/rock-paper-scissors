f42a0eb2b1dc2e23966c115e1f68fb4f15b0d66d
Evan Jones <evanjones125@gmail.com>
2022-03-26 14:19:02 -0600

    Generate number 1-3

--- script.js
+++ script.js
@@ -1,10 +1,22 @@
-//rock paper scissors
-
 const computerPlay = () => {
     //randomly return either rock, paper or scissors
-    //generate number between 1 and 3
-    //if 1, return rock, ...
+    //generate number between 0 and 1
+    //if num < 0.333, return rock, ...
+    const ranNum = Math.random();
+
+    if (ranNum <= 0.33333) {
+        return 1
+    } else if (ranNum >= 0.33334 && ranNum <= 0.66666) {
+        return 2;
+    } else if (ranNum >= 0.66667) {
+        return 3;
+    }
 }
+
+console.log(computerPlay());
+
+//get input from user; use it for playerSelection
+// let playerSelection = parseInt(prompt("enter your move: "))
 
 const playRound = (playerSelection, computerSelection) => {
     //takes in player and computer selection
@@ -12,3 +24,14 @@ const playRound = (playerSelection, computerSelection) => {
 
     //if pS beats cS, return "You win!"
 }
+
+const game = (playRound) => {
+    //calls playRound
+    //initiates a 5-round game
+    //keeps score
+    //outputs a report on who won or lost
+}
+
+const playerSelection = "rock";
+const computerSelection = computerPlay();
+console.log(playRound(playerSelection, computerSelection));
